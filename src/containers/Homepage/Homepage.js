6aa96c662401b8818b9cafd3b243b8373809bd85
Adam Drake <addrake@ebay.com>
2020-05-02 13:28:11 +0200

    updating homepage

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import People from '../../components/People';
+import Login from '../../components/Auth/Login';
+import { useUserContext } from '../../context/userContext';
 
 export default function Homepage() {
+  const { user } = useUserContext();
+
   return (
     <div className="container">
       <div>
         <h1>Cherish</h1>
-        <People />
+        {user ? <People /> : <Login />}
       </div>
     </div>
   );

b181b64ec03f3dd7b48da8e88df041c1060bfd65
Adam Drake <addrake@ebay.com>
2020-05-09 15:22:24 +0200

    Adding mobile menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,17 @@
-import React from 'react';
+/* eslint-disable react/jsx-curly-newline */
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useUserContext } from '../context/userContext';
 import Logout from './Auth/Logout';
 import UserAvatar from './UserAvatar';
 
 function Nav() {
+  const [isShowMenu, setIsShowMenu] = useState(false);
   const { user } = useUserContext();
 
   return (
     <div className="navigation-container">
-      <div className="navigation-list-container">
+      <nav className="navigation-list-container">
         <Link href="/">
           <a className="cherish-logo cherish-logo-nav">Cherish</a>
         </Link>
@@ -46,7 +48,104 @@ function Nav() {
           </li>
           <li>{user ? <UserAvatar displayName={user.displayName} /> : ''}</li>
         </ul>
-      </div>
+        <button
+          type="button"
+          className="mobile-menu-button"
+          onClick={() => setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)}
+        >
+          <svg width="30" height="30">
+            <path d="M0,5 30,5" stroke="#ff7474" strokeWidth="5" />
+            <path d="M0,14 30,14" stroke="#ff7474" strokeWidth="5" />
+            <path d="M0,23 30,23" stroke="#ff7474" strokeWidth="5" />
+          </svg>
+        </button>
+      </nav>
+      <nav className={`mobile-sidemenu-container ${isShowMenu ? 'show' : ''}`}>
+        <ul className="mobile-sidemenu-navigation-links">
+          <button
+            type="button"
+            className="btn-close"
+            onClick={() => setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)}
+          >
+            &times;
+          </button>
+          <li>{user ? <UserAvatar displayName={user.displayName} /> : ''}</li>
+          {user && (
+            <>
+              <li>
+                <Link href="/add-person">
+                  <a
+                    onClick={() =>
+                      setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)
+                    }
+                    onKeyPress={() =>
+                      setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)
+                    }
+                    role="button"
+                    tabIndex={0}
+                  >
+                    Add Person
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/calendar">
+                  <a
+                    onClick={() =>
+                      setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)
+                    }
+                    onKeyPress={() =>
+                      setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)
+                    }
+                    role="button"
+                    tabIndex={0}
+                  >
+                    Calendar
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
+          {!user && (
+            <li>
+              <Link href="/register">
+                <a
+                  onClick={() =>
+                    setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)
+                  }
+                  onKeyPress={() =>
+                    setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)
+                  }
+                  role="button"
+                  tabIndex={0}
+                >
+                  Register
+                </a>
+              </Link>
+            </li>
+          )}
+          <li>
+            {user ? (
+              <Logout />
+            ) : (
+              <Link href="/login">
+                <a
+                  onClick={() =>
+                    setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)
+                  }
+                  onKeyPress={() =>
+                    setIsShowMenu(prevIsShowMenu => !prevIsShowMenu)
+                  }
+                  role="button"
+                  tabIndex={0}
+                >
+                  Login
+                </a>
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }

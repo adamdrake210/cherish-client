69308164ae82a2e94737092e14fc5558bc7eccf7
Adam Drake <addrake@ebay.com>
2020-05-12 16:20:47 +0200

    update

---
+++ config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA4K4yoNB0H4DkOE0QLKJ0YyL6w-cHiiQ4',
+  authDomain: 'cherish-v2.firebaseapp.com',
+  databaseURL: 'https://cherish-v2.firebaseio.com',
+  projectId: 'cherish-v2',
+  storageBucket: 'cherish-v2.appspot.com',
+  messagingSenderId: process.env.REACT_API_APP_ID,
+  appId: process.env.REACT_API_APP_ID,
+};
+
+export default firebaseConfig;

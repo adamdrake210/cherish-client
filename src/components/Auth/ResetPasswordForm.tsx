f8783e7259c65a93e0e7941f0d606221f2cfe950
Adam Drake <addrake@ebay.com>
2021-10-09 13:29:49 +0200

    chore: restructure folders a bit for better org

---
+++ src/components/Auth/ResetPasswordForm.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik';
+import { firebase } from '@/services/firebase/firebase';
+import Email from '../Forms/Fields/Email';
+
+export default function ResetPasswordForm() {
+  const [firebaseError, setFirebaseError] = useState(null);
+  const [isPasswordReset, setIsPasswordReset] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  function resetPassword(email: string) {
+    setLoading(true);
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+  return (
+    <div className="login-container-form">
+      <Formik
+        initialValues={{
+          email: '',
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          resetPassword(values.email)
+            .then(response => {
+              setLoading(false);
+              setIsPasswordReset(true);
+              setSubmitting(false);
+              setFirebaseError(null);
+              console.error(response);
+            })
+            .catch(error => {
+              setLoading(false);
+              setSubmitting(false);
+              // Handle Errors here.
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              console.error('errorCode', errorCode);
+              console.error('errorMessage', errorMessage);
+              setFirebaseError(errorMessage);
+            });
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="formContainer">
+            <Email isEditable noLabel />
+
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="button button-lg button-blue m-t-5 m-b-20"
+            >
+              Reset Password
+            </button>
+          </Form>
+        )}
+      </Formik>
+      {loading && <p>Loading...</p>}
+      {isPasswordReset && <p>Check Email to reset password.</p>}
+      {firebaseError && <p>{firebaseError}</p>}
+    </div>
+  );
+}
